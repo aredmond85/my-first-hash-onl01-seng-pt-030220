3f1679b396c60e9316fcac4e528ae7386df64ce9
Austin Redmond <aredmond85@gmail.com>
2020-03-11 02:10:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice
-  my_hash = ["name"=> "Austin", "location" => "Kyle"]
+  my_hash = {"name" => "Austin", "age" => 34, "location" => "Kyle"}
 end
 
 
